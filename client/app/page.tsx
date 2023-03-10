062406a82684383c23a57e5895b74ef0c350467b
nikkhav <nikitakhavkin@gmail.com>
2023-03-10 18:01:29 +0100

    initial commit

---
+++ client/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+    </main>
+  );
+}
---
+++ client/app/welcome/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div>
+      <h2>Test</h2>
+    </div>
+  );
+};
+
+export default Welcome;
