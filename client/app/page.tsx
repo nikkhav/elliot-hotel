819cac9518d59b293391c2d52829e4f464aea4bd
nikkhav <nikitakhavkin@gmail.com>
2023-03-19 16:04:56 +0100

    Created beginning of the landing page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,35 @@
+import vacation from "../public/main-vacation.jpg";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main>
-      <h1>
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+      <title>Welcome to Elliot Hotel</title>
+      <div className={"relative text-center p-10"}>
+        <div className={"bg-black"}>
+          <Image
+            priority={true}
+            className={"opacity-50 transition-opacity duration-300"}
+            src={vacation}
+            alt={"Vacation"}
+          />
+          <h4
+            className={
+              "text-2xl text-white font-playfair tracking-wide absolute top-[40%] left-[50%] transform -translate-x-1/2 -translate-y-1/2"
+            }
+          >
+            Meet the Luxury
+          </h4>
+          <h3
+            className={
+              "text-6xl font-playfair text-white absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2"
+            }
+          >
+            {" "}
+            Добро пожаловать в <br /> Elliot Hotel!
+          </h3>
+        </div>
+      </div>
     </main>
   );
 }
